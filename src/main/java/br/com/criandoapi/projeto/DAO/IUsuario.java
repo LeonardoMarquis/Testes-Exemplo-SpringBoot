fdb9dfb1bd755dcde7215758497e451eba946f73
LeonardoMarquis <marquisleonardo12@gmail.com>
2025-09-28 12:18:05 -0300

    com dao interface usuario e usuario

---
+++ src/main/java/br/com/criandoapi/projeto/DAO/IUsuario.java
@@ -0,0 +1,7 @@
+package br.com.criandoapi.projeto.DAO;
+
+import org.springframework.data.repository.CrudRepository;
+import br.com.criandoapi.projeto.model.Usuario;
+
+public interface IUsuario extends CrudRepository<Usuario, Integer> {
+}
--- src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
+++ src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
@@ -1,12 +1,23 @@
 package br.com.criandoapi.projeto.controller;
 
+import java.util.List;
+
+import br.com.criandoapi.projeto.DAO.IUsuario;
+import br.com.criandoapi.projeto.model.Usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController                 // indica que essa classe é uma classe de controle, e nesse caso, um endpoint
 public class UsuarioController {
+
+    @Autowired
+    private IUsuario dao;
+
+
     @GetMapping("/usuarios")        // um endpoint no endereço /usuarios e agora quando acessa por: localhost:8080/usuarios, vai mostrar essa mensagem definida abaixo
-    public String texto(){
-        return "Usuario Controller";
+    public List<Usuario> listaUsuarios(){
+        return (List<Usuario>) dao.findAll();
     }
 }
