bad7c73a4578febaac1e6eaa9f5218b68f1a0a60
LeonardoMarquis <marquisleonardo12@gmail.com>
2025-09-28 16:38:49 -0300

    get post put delete

--- src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
+++ src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
@@ -1,23 +1,49 @@
 package br.com.criandoapi.projeto.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import br.com.criandoapi.projeto.DAO.IUsuario;
 import br.com.criandoapi.projeto.model.Usuario;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController                 // indica que essa classe é uma classe de controle, e nesse caso, um endpoint
+@RequestMapping("/usuarios")    // definindo aqui, os GetMapping terao essa rota como padrao, mas se quiser que seja diferente é so especificar no tal getmap
 public class UsuarioController {
 
     @Autowired
     private IUsuario dao;
 
 
-    @GetMapping("/usuarios")        // um endpoint no endereço /usuarios e agora quando acessa por: localhost:8080/usuarios, vai mostrar essa mensagem definida abaixo
+    @GetMapping        // um endpoint no endereço /usuarios e agora quando acessa por: localhost:8080/usuarios, vai mostrar essa mensagem definida abaixo
     public List<Usuario> listaUsuarios(){
         return (List<Usuario>) dao.findAll();
     }
+
+
+    @PostMapping            //esse endpoint sera para criar novo usuario para o BD
+    public Usuario criarUsuario(@RequestBody Usuario usuario){  //objeto do tipo Usuario sera salvo no banco
+        Usuario usuarioNovo = dao.save(usuario);
+        return usuarioNovo;
+    }
+
+
+    @PutMapping
+    public Usuario editarUsuario(@RequestBody Usuario usuario){
+        Usuario usuarioEditado = dao.save(usuario);
+        return usuarioEditado;
+    }
+
+    @DeleteMapping("/{id}")
+    public Optional<Usuario> excluirUsuario(@PathVariable Integer id){
+        Optional<Usuario> usuario = dao.findById(id);
+        dao.deleteById(id);
+        return usuario;
+
+    }
+
+
 }
